1097a0443776fb7e1a09cb06fe753b71d490420e
tofucodes <stellaferdinand1998@gmail.com>
2024-02-15 01:12:37 +0800

    updated setting screen

--- jobFinder-RNapp/component/SettingScreen.js
+++ jobFinder-RNapp/component/SettingScreen.js
@@ -1,18 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Button} from '@rneui/themed';
+
 
 export default function SettingScreen() {
     return(
-        <View style ={styles.container}>
-            <Text>Setting Screen</Text>
-        </View>
+        <View style={styles.container}>
+            <Button title = "Account" buttonStyle = {styles.button}></Button>
+            <Button title = "Notifications" buttonStyle = {styles.button}></Button>
+            <Button title = "Appearance" buttonStyle = {styles.button}></Button>
+            <Button title = "Help" buttonStyle = {styles.button}></Button>
+            <Button title = "About" buttonStyle = {styles.button}></Button>
+         </View>
+       
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#E6E6FA',
-      alignItems: 'center',
-      justifyContent: 'center',
+        marginTop:50
     },
-});
+    button: {
+        backgroundColor: '#696969',
+        color: 'black',
+        borderColor:'#696969', 
+        borderWidth:5,
+        margin: 10,
+        padding: 20
+    }});
